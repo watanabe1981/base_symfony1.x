23ae5f324369504778616fc6a5172eb6cc2e63bf
Isamu Watanabe <isamu.watanabe@baisound.com>
2014-02-15 05:05:18 +0900

    リファクタリング
    リファクタリング

--- sf14/apps/frontend/lib/myUser.class.php
+++ sf14/apps/frontend/lib/myUser.class.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * myUser 
+ * 
+ * @uses sfBasicSecurityUser
+ * @package 
+ * @version $id$
+ * @copyright Copyright (C) 2013 developpers.info All Rights Reserved.
+ * @author isamu.watanabe 
+ * @license PHP Version 5.0
+ */
 class myUser extends sfBasicSecurityUser
 {
 }
